7d4b5a48f34881d5cf5da2d6e2614e3f9603f7b2
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2017-02-11 00:09:12 +0100

    tiny fixes to please better code hub

--- bithug/config/environments/production.rb
+++ bithug/config/environments/production.rb
@@ -38,7 +38,6 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.action_mailer.perform_caching = false
 
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
