3cf3547bd15bbc65aa00a3f138d1cb6808e8d2ee
Djurre de Boer <djurre@almanapp.nl>
2017-02-10 20:49:39 +0100

    The test is running

--- test/TestContract.js
+++ test/TestContract.js
@@ -1,12 +1,11 @@
 var TestContract = artifacts.require("./TestContract.sol");
 contract('TestContract', function(accounts) {
   it("Test user account", function() {
-    // Get a reference to the deployed MetaCoin contract, as a JS object.
-    var meta = TestContract.deployed();
 
-    // Get the MetaCoin balance of the first account and assert that it's 10000.
-    return meta.getMe.call(accounts[0]).then(function(balance) {
-      assert.equal(balance.getValue(), accounts[0], "account is not equal");
+    return TestContract.deployed().then(function(instance) {
+      return instance.getMe.call();
+    }).then(function(balance) {
+      assert.equal(balance.valueOf(), accounts[1], "I'm not me!");
     });
   });
 });
