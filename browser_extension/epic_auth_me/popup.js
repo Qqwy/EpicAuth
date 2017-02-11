cafda6f45a1f353b905239d9c2caf435d7997e80
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2017-02-11 02:19:26 +0100

    more fields to show idea

--- browser_extension/epic_auth_me/popup.js
+++ browser_extension/epic_auth_me/popup.js
@@ -1,8 +1,8 @@
 
-self.resizeBy(100, 100);
+// self.resizeBy(100, 100);
 document.addEventListener('DOMContentLoaded', function() {
-    var height = document.body.offsetHeight;
+    // var height = document.body.offsetHeight;
     // self.resizeTo(500, height);
     console.log("TEST");
-    $(".ui.dropdown").dropdown();
+    $(".ui.dropdown").dropdown({direction: 'downward'});
 });
