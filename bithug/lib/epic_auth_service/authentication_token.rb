d18c85adbe0257b1ce80a7aef0c2016a5ee18452
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2017-02-11 03:56:05 +0100

    auth token encryption/decription works as planned.

--- bithug/lib/epic_auth_service/authentication_token.rb
+++ bithug/lib/epic_auth_service/authentication_token.rb
@@ -6,8 +6,9 @@ module EpicAuth
 
       attr_accessor :metadata, :user_id, :iat
 
-      def initialize(user_id, metadata = {}.with_indifferent_access)
+      def initialize(user_id, metadata = {}.with_indifferent_access, iat = nil)
         self.user_id = user_id
+        self.iat = iat
         self.metadata = metadata
         # TODO: Store in Blockchain.
       end
@@ -15,21 +16,25 @@ module EpicAuth
       def encrypt
         data = metadata
         data[:user_id] = user_id
-        data[:iat] = DateTime.now.utc.to_i
+        data[:iat] = (iat || DateTime.now.utc).to_i
         Base64.encode64(public_key.public_encrypt(data.to_json))
       end
 
       def self.decrypt(string)
-        self.metadata = JSON.parse(auth_token_private_key.private_decrypt(Base64.decode64(string))).with_indifferent_access
+        data = JSON.parse(private_key.private_decrypt(Base64.decode64(string))).with_indifferent_access
+        user_id = data[:user_id]
+        iat = data[:iat]
+        metadata = data.except(:user_id, :iat)
+        new(user_id, metadata, iat)
       end
 
       # FUTURE: Better security by encrypting key with pass stored in ENV.
-      def private_key
+      def self.private_key
         OpenSSL::PKey::RSA.new File.read 'crypto_keys/auth_token_private_key.pem'
       end
 
-      def public_key
-       private_key.public_key
+      def self.public_key
+        private_key.public_key
       end
     end
   end
