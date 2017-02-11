957fb4b31921cbf71bc4cf7e9d6c4a58ec6b251a
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2017-02-11 02:10:18 +0100

    working on browser extension look.

---
+++ browser_extension/epic_auth_me/popup.js
@@ -0,0 +1,8 @@
+
+self.resizeBy(100, 100);
+document.addEventListener('DOMContentLoaded', function() {
+    var height = document.body.offsetHeight;
+    // self.resizeTo(500, height);
+    console.log("TEST");
+    $(".ui.dropdown").dropdown();
+});
